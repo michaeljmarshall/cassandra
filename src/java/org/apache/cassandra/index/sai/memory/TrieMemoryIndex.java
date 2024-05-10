2fc6073ee991c83c6e5986d30a2df1311ddda130
Michael Marshall <mmarshall@apache.org>
2024-05-10 14:31:21 -0500

    Replace DA ref with DB

--- src/java/org/apache/cassandra/index/sai/memory/TrieMemoryIndex.java
+++ src/java/org/apache/cassandra/index/sai/memory/TrieMemoryIndex.java
@@ -219,7 +219,7 @@ public class TrieMemoryIndex extends MemoryIndex
         Trie<PrimaryKeys> subtrie = data.subtrie(lowerBound, lowerInclusive, upperBound, upperInclusive);
         if (!Version.LATEST.onOrAfter(Version.DB) && TypeUtil.isComposite(expression.validator))
             subtrie.entrySet().forEach(entry -> {
-                // Before version DA, we encoded composite types using a non order-preserving function. In order to
+                // Before version DB, we encoded composite types using a non order-preserving function. In order to
                 // perform a range query on a map, we use the bounds to get all entries for a given map key and then
                 // only keep the map entries that satisfy the expression.
                 ByteComparable unescaped = unescape(entry.getKey());
