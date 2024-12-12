michaeljmarshall/cassandra

commit 9cabec504d9f70ddfb345122dc4defed56a40f85

Squashed commit of the following:
commit 4c4c1ff802c7667e1289a4e730cc34bf6b3bd007
Author: Jonathan Ellis <jbellis@datastax.com>
Date:   Wed Jul 5 12:03:22 2023 -0500

    add test for creating index after data is added

commit 0acaae364c19ed7556b66b5576762ecc131af5fd
Author: Jeremiah D Jordan <jeremiah@datastax.com>
Date:   Wed Jul 5 09:23:01 2023 -0500

    Revert "enable segment compaction by default for vector indexes -- we should prioritize read performance"

    This reverts commit a4fa0728337ebf236ad03f6ad708cf51c3dc08c5.

commit e3ddb6426de4e89b5fe61ed742b69257865c2d3c
Author: Jonathan Ellis <jbellis@datastax.com>
Date:   Wed Jun 28 10:54:49 2023 -0500

    VECTOR-64 set sstable_growth to 0.5 by default

commit 3ca75046e6802ce5836a654459b01948c83d1d7b
Merge: a4fa072833 10a2a31ae8
Author: Jonathan Ellis <jbellis@datastax.com>
Date:   Wed Jun 28 10:54:18 2023 -0500

    merge ds-trunk

commit a4fa0728337ebf236ad03f6ad708cf51c3dc08c5
Author: Jonathan Ellis <jbellis@datastax.com>
Date:   Wed Jun 28 10:24:41 2023 -0500

    enable segment compaction by default for vector indexes -- we should prioritize read performance

commit 08153b38549d700505d064c25d7a2070c2ff07aa
Merge: 6a063b4f84 1a45fdc5f9
Author: Jonathan Ellis <jbellis@datastax.com>
Date:   Wed Jun 28 10:18:38 2023 -0500

    Merge branch 'vsearch' of github.com:datastax/cassandra into vsearch

commit 6a063b4f8458f119d4b99bd6b78cbc8832f27f13
Author: Jonathan Ellis <jbellis@datastax.com>
Date:   Wed Jun 28 10:13:51 2023 -0500

    VECTOR-63 add cassandra.sai.hnsw.allow_custom_parameters defaulting to false to control setting maximum_node_connections and construction_beam_width, which can cause memory usage to explode if used incorrectly

commit 1a45fdc5f92e9c82dba6e9e56a5e341806070b13
Author: Jeremiah D Jordan <jeremiah@datastax.com>
Date:   Mon Jun 26 15:57:10 2023 -0500

    Add the option to configure the file_cache_size_in_mb in a -D like it is in 6.8-cndb.

src/java/org/apache/cassandra/index/sai/disk/v1/bitpack/AbstractBlockPackedReader.java
@@ -17,10 +17,11 @@
  */
 package org.apache.cassandra.index.sai.disk.v1.bitpack;
 
-import org.apache.cassandra.index.sai.disk.v1.DirectReaders;
 import org.apache.cassandra.index.sai.disk.v1.LongArray;
 import org.apache.cassandra.index.sai.utils.SeekingRandomAccessInput;
 import org.apache.lucene.store.IndexInput;
+import org.apache.lucene.util.LongValues;
+import org.apache.lucene.util.packed.DirectReader;
 
 public abstract class AbstractBlockPackedReader implements LongArray
 {
@@ -58,8 +59,9 @@ public abstract class AbstractBlockPackedReader implements LongArray
 
         final int block = (int) (index >>> blockShift);
         final int idx = (int) (index & blockMask);
-        final DirectReaders.Reader subReader = DirectReaders.getReaderForBitsPerValue(blockBitsPerValue[block]);
-        return delta(block, idx) + subReader.get(input, blockOffsetAt(block), idx);
+        final LongValues subReader = blockBitsPerValue[block] == 0 ? LongValues.ZEROES
+                                                                   : DirectReader.getInstance(input, blockBitsPerValue[block], blockOffsetAt(block));
+        return delta(block, idx) + subReader.get(idx);
     }
 
     @Override

src/java/org/apache/cassandra/index/sai/disk/v1/kdtree/BKDReader.java
@@ -20,6 +20,7 @@ package org.apache.cassandra.index.sai.disk.v1.kdtree;
 import java.io.Closeable;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
@@ -38,7 +39,6 @@ import org.apache.cassandra.index.sai.IndexContext;
 import org.apache.cassandra.index.sai.QueryContext;
 import org.apache.cassandra.index.sai.disk.PostingList;
 import org.apache.cassandra.index.sai.disk.io.CryptoUtils;
-import org.apache.cassandra.index.sai.disk.v1.DirectReaders;
 import org.apache.cassandra.index.sai.disk.v1.postings.FilteringPostingList;
 import org.apache.cassandra.index.sai.disk.v1.postings.MergePostingList;
 import org.apache.cassandra.index.sai.disk.v1.postings.PostingsReader;
@@ -56,7 +56,8 @@ import org.apache.lucene.index.PointValues.Relation;
 import org.apache.lucene.store.IndexInput;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.FixedBitSet;
-import org.apache.lucene.util.FutureArrays;
+import org.apache.lucene.util.LongValues;
+import org.apache.lucene.util.packed.DirectReader;
 import org.apache.lucene.util.packed.DirectWriter;
 
 /**
@@ -74,7 +75,6 @@ public class BKDReader extends TraversingBKDReader implements Closeable
     private final FileHandle kdtreeFile;
     private final BKDPostingsIndex postingsIndex;
     private final ICompressor compressor;
-    private final DirectReaders.Reader leafOrderMapReader;
 
     /**
      * Performs a blocking read.
@@ -91,8 +91,6 @@ public class BKDReader extends TraversingBKDReader implements Closeable
         this.kdtreeFile = kdtreeFile;
         this.postingsIndex = new BKDPostingsIndex(postingsFile, bkdPostingsRoot);
         this.compressor = null;
-        final byte bits = (byte) DirectWriter.unsignedBitsRequired(maxPointsInLeafNode - 1);
-        leafOrderMapReader = DirectReaders.getReaderForBitsPerValue(bits);
     }
 
     public interface DocMapper
@@ -155,7 +153,7 @@ public class BKDReader extends TraversingBKDReader implements Closeable
         @Override
         public int compareTo(final IteratorState other)
         {
-            final int cmp = FutureArrays.compareUnsigned(scratch, 0, packedBytesLength, other.scratch, 0, packedBytesLength);
+            final int cmp = Arrays.compareUnsigned(scratch, 0, packedBytesLength, other.scratch, 0, packedBytesLength);
             if (cmp == 0)
             {
                 final long rowid1 = next;
@@ -251,7 +249,8 @@ public class BKDReader extends TraversingBKDReader implements Closeable
         final SeekingRandomAccessInput randoInput = new SeekingRandomAccessInput(bkdInput);
         for (int x = 0; x < count; x++)
         {
-            final short idx = (short) LeafOrderMap.getValue(randoInput, orderMapPointer, x, leafOrderMapReader);
+            LongValues orderMapReader = DirectReader.getInstance(randoInput, DirectWriter.unsignedBitsRequired(maxPointsInLeafNode - 1), orderMapPointer);
+            final short idx = (short) LeafOrderMap.getValue(x, orderMapReader);
             origIndex[x] = idx;
         }
 
@@ -706,7 +705,8 @@ public class BKDReader extends TraversingBKDReader implements Closeable
             final SeekingRandomAccessInput randoInput = new SeekingRandomAccessInput(bkdInput);
             for (int x = 0; x < count; x++)
             {
-                origIndex[x] = (short) LeafOrderMap.getValue(randoInput, orderMapPointer, x, leafOrderMapReader);
+                LongValues orderMapReader = DirectReader.getInstance(randoInput, DirectWriter.unsignedBitsRequired(maxPointsInLeafNode - 1), orderMapPointer);
+                origIndex[x] = (short) orderMapReader.get(x);
             }
 
             // seek beyond the ordermap
@@ -743,8 +743,8 @@ public class BKDReader extends TraversingBKDReader implements Closeable
             assert splitDimValue.length == bytesPerDim;
 
             // make sure cellMin <= splitValue <= cellMax:
-            assert FutureArrays.compareUnsigned(cellMinPacked, splitDim * bytesPerDim, splitDim * bytesPerDim + bytesPerDim, splitDimValue.bytes, splitDimValue.offset, splitDimValue.offset + bytesPerDim) <= 0 : "bytesPerDim=" + bytesPerDim + " splitDim=" + splitDim + " numDims=" + numDims;
-            assert FutureArrays.compareUnsigned(cellMaxPacked, splitDim * bytesPerDim, splitDim * bytesPerDim + bytesPerDim, splitDimValue.bytes, splitDimValue.offset, splitDimValue.offset + bytesPerDim) >= 0 : "bytesPerDim=" + bytesPerDim + " splitDim=" + splitDim + " numDims=" + numDims;
+            assert Arrays.compareUnsigned(cellMinPacked, splitDim * bytesPerDim, splitDim * bytesPerDim + bytesPerDim, splitDimValue.bytes, splitDimValue.offset, splitDimValue.offset + bytesPerDim) <= 0 : "bytesPerDim=" + bytesPerDim + " splitDim=" + splitDim + " numDims=" + numDims;
+            assert Arrays.compareUnsigned(cellMaxPacked, splitDim * bytesPerDim, splitDim * bytesPerDim + bytesPerDim, splitDimValue.bytes, splitDimValue.offset, splitDimValue.offset + bytesPerDim) >= 0 : "bytesPerDim=" + bytesPerDim + " splitDim=" + splitDim + " numDims=" + numDims;
 
             // Recurse on left sub-tree:
             System.arraycopy(cellMaxPacked, 0, splitPackedValue, 0, packedBytesLength);

src/java/org/apache/cassandra/index/sai/disk/v1/kdtree/LeafOrderMap.java
@@ -19,16 +19,15 @@ package org.apache.cassandra.index.sai.disk.v1.kdtree;
 
 import java.io.IOException;
 
-import org.apache.cassandra.index.sai.disk.v1.DirectReaders;
 import org.apache.lucene.store.DataOutput;
-import org.apache.lucene.store.RandomAccessInput;
+import org.apache.lucene.util.LongValues;
 import org.apache.lucene.util.packed.DirectWriter;
 
 public class LeafOrderMap
 {
-    public static int getValue(RandomAccessInput in, long offset, int index, DirectReaders.Reader reader)
+    public static int getValue(int index, LongValues reader)
     {
-        return Math.toIntExact(reader.get(in, offset, index));
+        return Math.toIntExact(reader.get(index));
     }
 
     public static void write(final int[] array, int length, int maxValue, final DataOutput out) throws IOException

src/java/org/apache/cassandra/index/sai/disk/v1/kdtree/TraversingBKDReader.java
@@ -18,6 +18,7 @@
 package org.apache.cassandra.index.sai.disk.v1.kdtree;
 
 import java.io.Closeable;
+import java.util.Arrays;
 
 import org.agrona.collections.IntArrayList;
 import org.apache.cassandra.index.sai.disk.io.IndexInputReader;
@@ -30,7 +31,6 @@ import org.apache.cassandra.utils.Throwables;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.store.ByteArrayDataInput;
 import org.apache.lucene.util.BytesRef;
-import org.apache.lucene.util.FutureArrays;
 import org.apache.lucene.util.MathUtil;
 
 /**
@@ -82,7 +82,7 @@ public class TraversingBKDReader implements Closeable
 
             for (int dim = 0; dim < numDims; dim++)
             {
-                if (FutureArrays.compareUnsigned(minPackedValue, dim * bytesPerDim, dim * bytesPerDim + bytesPerDim, maxPackedValue, dim * bytesPerDim, dim * bytesPerDim + bytesPerDim) > 0)
+                if (Arrays.compareUnsigned(minPackedValue, dim * bytesPerDim, dim * bytesPerDim + bytesPerDim, maxPackedValue, dim * bytesPerDim, dim * bytesPerDim + bytesPerDim) > 0)
                 {
                     String message = String.format("Min packed value %s is > max packed value %s for dimension %d.",
                                                    new BytesRef(minPackedValue), new BytesRef(maxPackedValue), dim);

commit 80e0d45476f18f541e035e10ed9c4ed1f13a19f4

SAI: Reduce DirectReader object creation (#1011)
This performance regression was introduced by
9cabec504d9f70ddfb345122dc4defed56a40f85. I haven't been able to find
the exact commit responsible for this regression, but essentially, we
switched from `DirectReaders` to the lucene `DirectReader` around
07743459e33ff0fe9e3628a70e54f0ab3ed1f50a and
6bd00d1dbc2ade6b71aae4fcbe456713e0d2a251, and that led to a lot of
unnecessary object creation.

In my initial testing, this change appeared to improve numeric query
throughput from `282.03it/s` for 100k queries to `333.15it/s` for 100k
queries. The memory profile also showed far fewer objects created.

Note: the above numbers might have been from variability in my testing.
It would be helpful to test in a more controlled environment. Either
way, based on my understanding of the objects involved, this should
generally produce better results because we'll have fewer objects.

Finally, this change is especially helpful because the `sort then
filter` logic has to initialize the the iterators for search, so it
helps in both execution paths.

src/java/org/apache/cassandra/index/sai/disk/v1/bitpack/AbstractBlockPackedReader.java
@@ -17,6 +17,7 @@
  */
 package org.apache.cassandra.index.sai.disk.v1.bitpack;
 
+import com.carrotsearch.hppc.IntObjectHashMap;
 import org.apache.cassandra.index.sai.disk.io.IndexInput;
 import org.apache.cassandra.index.sai.disk.oldlucene.LuceneCompat;
 import org.apache.cassandra.index.sai.disk.v1.LongArray;
@@ -31,6 +32,7 @@ public abstract class AbstractBlockPackedReader implements LongArray
     private final long valueCount;
     final byte[] blockBitsPerValue; // package protected for test access
     private final SeekingRandomAccessInput input;
+    private final IntObjectHashMap<LongValues> readers;
 
     private long prevTokenValue = Long.MIN_VALUE;
     private long lastIndex; // the last index visited by token -> row ID searches
@@ -43,6 +45,7 @@ public abstract class AbstractBlockPackedReader implements LongArray
         this.valueCount = valueCount;
         this.input = new SeekingRandomAccessInput(indexInput);
         this.blockBitsPerValue = blockBitsPerValue;
+        this.readers = new IntObjectHashMap<>();
         // start searching tokens from current index segment
         this.lastIndex = sstableRowId;
     }
@@ -59,9 +62,19 @@ public abstract class AbstractBlockPackedReader implements LongArray
 
         final int block = (int) (index >>> blockShift);
         final int idx = (int) (index & blockMask);
-        final LongValues subReader = blockBitsPerValue[block] == 0 ? LongValues.ZEROES
-                                                                   : LuceneCompat.directReaderGetInstance(input, blockBitsPerValue[block], blockOffsetAt(block));
-        return delta(block, idx) + subReader.get(idx);
+        return delta(block, idx) + getReader(block).get(idx);
+    }
+
+    private LongValues getReader(int block)
+    {
+        LongValues reader = readers.get(block);
+        if (reader == null)
+        {
+            reader = blockBitsPerValue[block] == 0 ? LongValues.ZEROES
+                                                   : LuceneCompat.directReaderGetInstance(input, blockBitsPerValue[block], blockOffsetAt(block));
+            readers.put(block, reader);
+        }
+        return reader;
     }
 
     @Override

src/java/org/apache/cassandra/index/sai/disk/v1/kdtree/BKDReader.java
@@ -221,10 +221,10 @@ public class BKDReader extends TraversingBKDReader implements Closeable
         final byte[] scratchPackedValue1 = new byte[packedBytesLength];
 
         final SeekingRandomAccessInput randoInput = new SeekingRandomAccessInput(bkdInput);
+        LongValues orderMapReader = LuceneCompat.directReaderGetInstance(randoInput, bitsPerValue, orderMapPointer);
         for (int x = 0; x < count; x++)
         {
-            LongValues orderMapReader = LuceneCompat.directReaderGetInstance(randoInput, LuceneCompat.directWriterUnsignedBitsRequired(randoInput.order(), maxPointsInLeafNode - 1), orderMapPointer);
-            final short idx = (short) LeafOrderMap.getValue(x, orderMapReader);
+            final short idx = LeafOrderMap.getValue(x, orderMapReader);
             origIndex[x] = idx;
         }
 
@@ -343,6 +343,7 @@ public class BKDReader extends TraversingBKDReader implements Closeable
         final QueryContext context;
 
         final IndexInput bkdInput;
+        final SeekingRandomAccessInput bkdRandomInput;
         final IndexInput postingsInput;
         final IndexInput postingsSummaryInput;
         final IndexTree index;
@@ -352,6 +353,7 @@ public class BKDReader extends TraversingBKDReader implements Closeable
                      IndexTree index, QueryEventListener.BKDIndexEventListener listener, QueryContext context)
         {
             this.bkdInput = bkdInput;
+            this.bkdRandomInput = new SeekingRandomAccessInput(bkdInput);
             this.postingsInput = postingsInput;
             this.postingsSummaryInput = postingsSummaryInput;
             this.index = index;
@@ -670,11 +672,10 @@ public class BKDReader extends TraversingBKDReader implements Closeable
 
             final long orderMapPointer = bkdInput.getFilePointer();
 
-            final SeekingRandomAccessInput randoInput = new SeekingRandomAccessInput(bkdInput);
+            LongValues orderMapReader = LuceneCompat.directReaderGetInstance(bkdRandomInput, bitsPerValue, orderMapPointer);
             for (int x = 0; x < count; x++)
             {
-                LongValues orderMapReader = LuceneCompat.directReaderGetInstance(randoInput, LuceneCompat.directWriterUnsignedBitsRequired(randoInput.order(), maxPointsInLeafNode - 1), orderMapPointer);
-                origIndex[x] = (short) orderMapReader.get(x);
+                origIndex[x] = LeafOrderMap.getValue(x, orderMapReader);
             }
 
             // seek beyond the ordermap

src/java/org/apache/cassandra/index/sai/disk/v1/kdtree/LeafOrderMap.java
@@ -27,9 +27,21 @@ import org.apache.lucene.util.LongValues;
 
 public class LeafOrderMap
 {
-    public static int getValue(int index, LongValues reader)
+    /**
+     * Get the value at the given index from the reader, and cast it to a short. If the value is too large to fit in a
+     * short, an ArithmeticException is thrown.
+     * @param index the index to read from
+     * @param reader the reader to read from
+     * @return the value at the given index, cast to a short
+     */
+    public static short getValue(int index, LongValues reader)
     {
-        return Math.toIntExact(reader.get(index));
+        var value = reader.get(index);
+        var result = (short) value;
+        if (result != value) {
+            throw new ArithmeticException("short overflow");
+        }
+        return result;
     }
 
     public static void write(ByteOrder order, final int[] array, int length, int maxValue, final DataOutput out) throws IOException

src/java/org/apache/cassandra/index/sai/disk/v1/kdtree/TraversingBKDReader.java
@@ -22,6 +22,7 @@ import java.util.Arrays;
 
 import org.agrona.collections.IntArrayList;
 import org.apache.cassandra.index.sai.disk.io.IndexInputReader;
+import org.apache.cassandra.index.sai.disk.oldlucene.LuceneCompat;
 import org.apache.cassandra.index.sai.utils.SAICodecUtils;
 import org.apache.cassandra.io.util.FileHandle;
 import org.apache.cassandra.io.util.FileUtils;
@@ -31,6 +32,7 @@ import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.store.ByteArrayDataInput;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.MathUtil;
+import org.apache.lucene.util.packed.DirectWriter;
 
 /**
  * Base reader for a block KD-tree previously written with {@link BKDWriter}.
@@ -50,6 +52,7 @@ public class TraversingBKDReader implements Closeable
     final int leafNodeOffset;
     final int numDims;
     final int maxPointsInLeafNode;
+    final int bitsPerValue;
     final int packedBytesLength;
 
     @SuppressWarnings("resource")
@@ -64,6 +67,7 @@ public class TraversingBKDReader implements Closeable
 
             numDims = in.readVInt();
             maxPointsInLeafNode = in.readVInt();
+            bitsPerValue = LuceneCompat.directWriterUnsignedBitsRequired(in.order(), maxPointsInLeafNode - 1);
             bytesPerDim = in.readVInt();
             packedBytesLength = numDims * bytesPerDim;
 