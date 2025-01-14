michaeljmarshall/cassandra

commit 338902ce0849e2e6f9b11a689a9c478e0b20ad6c

CNDB-9850: Row-level trie memtable with DeletionInfo tombstone handling
Implements Stage 2 of Trie memtables, implementing trie-backed partitions
and extending the memtable map to the level of tries. This stage still
handles deletions with the legacy mechanisms (RangeTombstoneList etc)
but can save quite a bit of space for the B-Tree partition-to-row maps.

Also includes:
- Code alignment with the Cassandra 5.0 branch.
- A port of the OSS50 byte-comparable encoding version.
- Fixed version preencoded byte-comparables for version safety.
- Duplication of byte sources and better toArray conversion.
- Direct skipping mechanism for trie cursors and map-like get method.
- Forced node copying mechanism for trie mutations for atomicity and consistency.
- Pluggable cell reuse.
- Prefix and tail tries, as well as filtered iteration.
- A mechanism for extracting current key during trie mutation.
- Volatile reads to fully enforce happens-before between node preparation and use.
- Various in-memory trie improvements.

The earlier trie memtable implementation is still available as TrieMemtableStage1.

src/java/org/apache/cassandra/db/ClusteringComparator.java
@@ -26,11 +26,11 @@ import java.util.Objects;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 
+import org.apache.cassandra.db.marshal.AbstractType;
 import org.apache.cassandra.db.marshal.ValueAccessor;
 import org.apache.cassandra.db.rows.Row;
-import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.io.sstable.format.big.IndexInfo;
 import org.apache.cassandra.serializers.MarshalException;
-
 import org.apache.cassandra.utils.bytecomparable.ByteComparable;
 import org.apache.cassandra.utils.bytecomparable.ByteSource;
 
@@ -38,8 +38,8 @@ import static org.apache.cassandra.utils.bytecomparable.ByteSource.EXCLUDED;
 import static org.apache.cassandra.utils.bytecomparable.ByteSource.NEXT_COMPONENT;
 import static org.apache.cassandra.utils.bytecomparable.ByteSource.NEXT_COMPONENT_NULL;
 import static org.apache.cassandra.utils.bytecomparable.ByteSource.NEXT_COMPONENT_NULL_REVERSED;
+import static org.apache.cassandra.utils.bytecomparable.ByteSource.NEXT_CLUSTERING_NULL;
 import static org.apache.cassandra.utils.bytecomparable.ByteSource.TERMINATOR;
-import org.apache.cassandra.io.sstable.format.big.IndexInfo;
 
 /**
  * A comparator of clustering prefixes (or more generally of {@link Clusterable}}.
@@ -293,6 +293,7 @@ public class ClusteringComparator implements Comparator<Clusterable>
                 {
                     if (current != null)
                     {
+                        // Process bytes of the current component.
                         int b = current.next();
                         if (b > END_OF_STREAM)
                             return b;
@@ -300,20 +301,45 @@ public class ClusteringComparator implements Comparator<Clusterable>
                     }
 
                     int sz = src.size();
-                    if (srcnum == sz)
+                    if (srcnum == sz) // already produced the Kind byte, we are done
                         return END_OF_STREAM;
 
                     ++srcnum;
                     if (srcnum == sz)
                         return src.kind().asByteComparableValue(version);
+                    else
+                        return advanceToComponent(src.get(srcnum));
+                }
 
-                    current = subtype(srcnum).asComparableBytes(src.accessor(), src.get(srcnum), version);
+                private int advanceToComponent(V nextComponent)
+                {
+                    // We can have a null as the clustering component (this is a relic of COMPACT STORAGE, but also
+                    // can appear in indexed partitions with no rows but static content),
+                    if (nextComponent == null)
+                    {
+                        if (version == Version.OSS50)
+                            return NEXT_CLUSTERING_NULL; // always sorts before non-nulls, including for reversed types
+                        else
+                        {
+                            // legacy version did not permit nulls in clustering keys and treated these as null values
+                            return nextComponentNull(subtype(srcnum).isReversed());
+                        }
+                    }
+
+                    current = subtype(srcnum).asComparableBytes(src.accessor(), nextComponent, version);
+                    // and also null values for some types (e.g. int, varint but not text) that are encoded as empty
+                    // buffers.
                     if (current == null)
-                        return subtype(srcnum).isReversed() ? NEXT_COMPONENT_NULL_REVERSED : NEXT_COMPONENT_NULL;
+                        return nextComponentNull(subtype(srcnum).isReversed());
 
                     return NEXT_COMPONENT;
                 }
             };
+        }
+
+        private int nextComponentNull(boolean isReversed)
+        {
+            return isReversed ? NEXT_COMPONENT_NULL_REVERSED : NEXT_COMPONENT_NULL;
         }
 
         public String toString()
@@ -335,7 +361,7 @@ public class ClusteringComparator implements Comparator<Clusterable>
     public <V> Clustering<?> clusteringFromByteComparable(ValueAccessor<V> accessor,
                                                           ByteComparable comparable)
     {
-        ByteComparable.Version version = ByteComparable.Version.OSS41;
+        ByteComparable.Version version = ByteComparable.Version.OSS50;
         ByteSource.Peekable orderedBytes = ByteSource.peekable(comparable.asComparableBytes(version));
         if (orderedBytes == null)
             return null;
@@ -360,11 +386,15 @@ public class ClusteringComparator implements Comparator<Clusterable>
         {
             switch (sep)
             {
+            case NEXT_CLUSTERING_NULL:
+                components[cc] = null;
+                break;
             case NEXT_COMPONENT_NULL:
             case NEXT_COMPONENT_NULL_REVERSED:
-                components[cc] = accessor.empty();
+                components[cc] = subtype(cc).fromComparableBytes(accessor, null, version);
                 break;
             case NEXT_COMPONENT:
+                // Decode the next component, consuming bytes from orderedBytes.
                 components[cc] = subtype(cc).fromComparableBytes(accessor, orderedBytes, version);
                 break;
             case TERMINATOR:
@@ -389,9 +419,7 @@ public class ClusteringComparator implements Comparator<Clusterable>
      * for a exclusive end and an inclusive start is the same, before the exact clustering). The type must be supplied
      * separately (in the bound... vs boundary... call and isEnd argument).
      *
-     * @param accessor Accessor to use to construct components. Because this will be used to construct individual
-     *                 arrays/buffers for each component, it may be sensible to use an accessor that allocates larger
-     *                 buffers in advance.
+     * @param accessor Accessor to use to construct components.
      * @param comparable The clustering position encoded as a byte-comparable sequence.
      * @param isEnd true if the bound marks the end of a range, false is it marks the start.
      */
@@ -399,10 +427,8 @@ public class ClusteringComparator implements Comparator<Clusterable>
                                                           ByteComparable comparable,
                                                           boolean isEnd)
     {
-        ByteComparable.Version version = ByteComparable.Version.OSS41;
+        ByteComparable.Version version = ByteComparable.Version.OSS50;
         ByteSource.Peekable orderedBytes = ByteSource.peekable(comparable.asComparableBytes(version));
-        if (orderedBytes == null)
-            return null;
 
         int sep = orderedBytes.next();
         int cc = 0;
@@ -412,11 +438,15 @@ public class ClusteringComparator implements Comparator<Clusterable>
         {
             switch (sep)
             {
+            case NEXT_CLUSTERING_NULL:
+                components[cc] = null;
+                break;
             case NEXT_COMPONENT_NULL:
             case NEXT_COMPONENT_NULL_REVERSED:
-                components[cc] = accessor.empty();
+                components[cc] = subtype(cc).fromComparableBytes(accessor, null, version);
                 break;
             case NEXT_COMPONENT:
+                // Decode the next component, consuming bytes from orderedBytes.
                 components[cc] = subtype(cc).fromComparableBytes(accessor, orderedBytes, version);
                 break;
             case ByteSource.LT_NEXT_COMPONENT:
@@ -449,20 +479,14 @@ public class ClusteringComparator implements Comparator<Clusterable>
      * for a exclusive end and an inclusive start is the same, before the exact clustering). The type must be supplied
      * separately (in the bound... vs boundary... call and isEnd argument).
      *
-     * @param accessor Accessor to use to construct components. Because this will be used to construct individual
-     *                 arrays/buffers for each component, it may be sensible to use an accessor that allocates larger
-     *                 buffers in advance.
+     * @param accessor Accessor to use to construct components.
      * @param comparable The clustering position encoded as a byte-comparable sequence.
      */
-    public <V> ClusteringBoundary<V> boundaryFromByteComparable(ValueAccessor<V> accessor,
-                                                                ByteComparable comparable)
+    public <V> ClusteringBoundary<V> boundaryFromByteComparable(ValueAccessor<V> accessor, ByteComparable comparable)
     {
-        ByteComparable.Version version = ByteComparable.Version.OSS41;
+        ByteComparable.Version version = ByteComparable.Version.OSS50;
         ByteSource.Peekable orderedBytes = ByteSource.peekable(comparable.asComparableBytes(version));
-        if (orderedBytes == null)
-            return null;
 
-        // First check for special cases (partition key only, static clustering) that can do without buffers.
         int sep = orderedBytes.next();
         int cc = 0;
         V[] components = accessor.createArray(size());
@@ -471,11 +495,15 @@ public class ClusteringComparator implements Comparator<Clusterable>
         {
             switch (sep)
             {
+            case NEXT_CLUSTERING_NULL:
+                components[cc] = null;
+                break;
             case NEXT_COMPONENT_NULL:
             case NEXT_COMPONENT_NULL_REVERSED:
-                components[cc] = accessor.empty();
+                components[cc] = subtype(cc).fromComparableBytes(accessor, null, version);
                 break;
             case NEXT_COMPONENT:
+                // Decode the next component, consuming bytes from orderedBytes.
                 components[cc] = subtype(cc).fromComparableBytes(accessor, orderedBytes, version);
                 break;
             case ByteSource.LT_NEXT_COMPONENT:

src/java/org/apache/cassandra/index/sai/disk/v2/RowAwarePrimaryKeyMap.java
@@ -32,8 +32,8 @@ import org.apache.cassandra.db.marshal.ByteBufferAccessor;
 import org.apache.cassandra.dht.IPartitioner;
 import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.index.sai.disk.PrimaryKeyMap;
-import org.apache.cassandra.index.sai.disk.format.IndexComponents;
 import org.apache.cassandra.index.sai.disk.format.IndexComponentType;
+import org.apache.cassandra.index.sai.disk.format.IndexComponents;
 import org.apache.cassandra.index.sai.disk.v1.LongArray;
 import org.apache.cassandra.index.sai.disk.v1.MetadataSource;
 import org.apache.cassandra.index.sai.disk.v1.bitpack.BlockPackedReader;
@@ -41,12 +41,12 @@ import org.apache.cassandra.index.sai.disk.v1.bitpack.NumericValuesMeta;
 import org.apache.cassandra.index.sai.disk.v2.sortedterms.SortedTermsMeta;
 import org.apache.cassandra.index.sai.disk.v2.sortedterms.SortedTermsReader;
 import org.apache.cassandra.index.sai.utils.PrimaryKey;
+import org.apache.cassandra.index.sai.utils.TypeUtil;
 import org.apache.cassandra.io.sstable.SSTableId;
 import org.apache.cassandra.io.sstable.format.SSTableReader;
 import org.apache.cassandra.io.util.FileHandle;
 import org.apache.cassandra.io.util.FileUtils;
 import org.apache.cassandra.utils.Throwables;
-import org.apache.cassandra.utils.bytecomparable.ByteComparable;
 import org.apache.cassandra.utils.bytecomparable.ByteSource;
 import org.apache.cassandra.utils.bytecomparable.ByteSourceInverse;
 
@@ -253,10 +253,10 @@ public class RowAwarePrimaryKeyMap implements PrimaryKeyMap
         try
         {
             cursor.seekToPointId(sstableRowId);
-            ByteSource.Peekable peekable = cursor.term().asPeekableBytes(ByteComparable.Version.OSS41);
+            ByteSource.Peekable peekable = cursor.term().asPeekableBytes(TypeUtil.BYTE_COMPARABLE_VERSION);
 
             Token token = partitioner.getTokenFactory().fromComparableBytes(ByteSourceInverse.nextComponentSource(peekable),
-                                                                            ByteComparable.Version.OSS41);
+                                                                            TypeUtil.BYTE_COMPARABLE_VERSION);
             byte[] keyBytes = ByteSourceInverse.getUnescapedBytes(ByteSourceInverse.nextComponentSource(peekable));
 
             if (keyBytes == null)

commit 19c2ed43deba3413e7c556c29242f370d161537e

CNDB-12445: parameterize ByteComparable.Version for clusteringFromByteComparable (#1504)
### What is the issue
Fixes: https://github.com/riptano/cndb/issues/12445

### What does this PR fix and why was it fixed
Appears to be a regression introduced by https://github.com/datastax/cassandra/commit/338902ce0849e2e6f9b11a689a9c478e0b20ad6c or https://github.com/datastax/cassandra/pull/1177

The general issue is that SAI uses `OSS41` to encode its primary keys in the trie index, but this code path attempted to interpret the clustering columns using `OSS50`.

src/java/org/apache/cassandra/db/ClusteringComparator.java
@@ -381,7 +381,7 @@ public class ClusteringComparator implements Comparator<Clusterable>
     /**
      * Produces a clustering from the given byte-comparable value. The method will throw an exception if the value
      * does not correctly encode a clustering of this type, including if it encodes a position before or after a
-     * clustering (i.e. a bound/boundary).
+     * clustering (i.e. a bound/boundary). Uses the OSS50 version of the byte-comparable encoding.
      *
      * @param accessor Accessor to use to construct components. Because this will be used to construct individual
      *                 arrays/buffers for each component, it may be sensible to use an accessor that allocates larger
@@ -391,7 +391,24 @@ public class ClusteringComparator implements Comparator<Clusterable>
     public <V> Clustering<?> clusteringFromByteComparable(ValueAccessor<V> accessor,
                                                           ByteComparable comparable)
     {
-        ByteComparable.Version version = ByteComparable.Version.OSS50;
+        return clusteringFromByteComparable(accessor, comparable, ByteComparable.Version.OSS50);
+    }
+
+    /**
+     * Produces a clustering from the given byte-comparable value. The method will throw an exception if the value
+     * does not correctly encode a clustering of this type, including if it encodes a position before or after a
+     * clustering (i.e. a bound/boundary). Uses the OSS50 version of the byte-comparable encoding.
+     *
+     * @param accessor Accessor to use to construct components. Because this will be used to construct individual
+     *                 arrays/buffers for each component, it may be sensible to use an accessor that allocates larger
+     *                 buffers in advance.
+     * @param comparable The clustering encoded as a byte-comparable sequence.
+     * @param version The version of the byte-comparable encoding.
+     */
+    public <V> Clustering<?> clusteringFromByteComparable(ValueAccessor<V> accessor,
+                                                          ByteComparable comparable,
+                                                          ByteComparable.Version version)
+    {
         ByteSource.Peekable orderedBytes = ByteSource.peekable(comparable.asComparableBytes(version));
         if (orderedBytes == null)
             return null;

src/java/org/apache/cassandra/index/sai/disk/v2/RowAwarePrimaryKeyMap.java
@@ -275,7 +275,8 @@ public class RowAwarePrimaryKeyMap implements PrimaryKeyMap
             Clustering clustering = clusteringComparator.size() == 0
                                     ? Clustering.EMPTY
                                     : clusteringComparator.clusteringFromByteComparable(ByteBufferAccessor.instance,
-                                                                                        v -> ByteSourceInverse.nextComponentSource(peekable));
+                                                                                        v -> ByteSourceInverse.nextComponentSource(peekable),
+                                                                                        TypeUtil.BYTE_COMPARABLE_VERSION);
 
             return primaryKeyFactory.create(partitionKey, clustering);
         }

test/unit/org/apache/cassandra/index/sai/cql/ClusteringKeyIndexTest.java
@@ -29,24 +29,25 @@ public class ClusteringKeyIndexTest extends SAITester
     @Before
     public void createTableAndIndex()
     {
-        createTable("CREATE TABLE %s (pk1 int, pk2 text, val int, PRIMARY KEY((pk1), pk2)) WITH CLUSTERING ORDER BY (pk2 DESC)");
+        createTable("CREATE TABLE %s (pk1 int, pk2 text, val int, val2 int, PRIMARY KEY((pk1), pk2)) WITH CLUSTERING ORDER BY (pk2 DESC)");
         createIndex("CREATE CUSTOM INDEX pk2_idx ON %s(pk2) USING 'StorageAttachedIndex'");
+        createIndex("CREATE CUSTOM INDEX val2_idx ON %s(val2) USING 'StorageAttachedIndex'");
 
         disableCompaction();
     }
 
     private void insertData1() throws Throwable
     {
-        execute("INSERT INTO %s (pk1, pk2, val) VALUES (1, '1', 1)");
-        execute("INSERT INTO %s (pk1, pk2, val) VALUES (2, '2', 2)");
-        execute("INSERT INTO %s (pk1, pk2, val) VALUES (3, '3', 3)");
+        execute("INSERT INTO %s (pk1, pk2, val, val2) VALUES (1, '1', 1, 1)");
+        execute("INSERT INTO %s (pk1, pk2, val, val2) VALUES (2, '2', 2, 2)");
+        execute("INSERT INTO %s (pk1, pk2, val, val2) VALUES (3, '3', 3, 3)");
     }
 
     private void insertData2() throws Throwable
     {
-        execute("INSERT INTO %s (pk1, pk2, val) VALUES (4, '4', 4)");
-        execute("INSERT INTO %s (pk1, pk2, val) VALUES (5, '5', 5)");
-        execute("INSERT INTO %s (pk1, pk2, val) VALUES (6, '6', 6)");
+        execute("INSERT INTO %s (pk1, pk2, val, val2) VALUES (4, '4', 4, 4)");
+        execute("INSERT INTO %s (pk1, pk2, val, val2) VALUES (5, '5', 5, 5)");
+        execute("INSERT INTO %s (pk1, pk2, val, val2) VALUES (6, '6', 6, 6)");
     }
 
     @Test
@@ -98,7 +99,7 @@ public class ClusteringKeyIndexTest extends SAITester
 
     private Object[] expectedRow(int index)
     {
-        return row(index, Integer.toString(index), index);
+        return row(index, Integer.toString(index), index, index);
     }
 
     private void runQueries() throws Throwable
@@ -111,5 +112,7 @@ public class ClusteringKeyIndexTest extends SAITester
 
         assertThatThrownBy(()->execute("SELECT * FROM %s WHERE pk1 = -1 AND val = 2")).hasMessageContaining("use ALLOW FILTERING");
 
+        // Add an assertion that covers searching a non-primary key column
+        assertRowsIgnoringOrder(execute("SELECT * FROM %s WHERE val2 = 1"), expectedRow(1));
     }
 }