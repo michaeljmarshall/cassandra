e413cf0249f63db0ce07ec521e02a92465fda19c
Michael Marshall <mmarshall@apache.org>
2026-02-24 16:26:51 -0600

    Add CellSourceIdentifier interface to simplify implementation
    (cherry picked from commit 140613be9aa62f456a18317cc02613e96d888dbb)

--- src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
+++ src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
@@ -734,7 +734,7 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
 
     public UnfilteredRowIterator queryMemtableAndDisk(ColumnFamilyStore cfs,
                                                       ColumnFamilyStore.ViewFragment view,
-                                                      Function<Object, Transformation<BaseRowIterator<?>>> rowTransformer,
+                                                      Function<CellSourceIdentifier, Transformation<BaseRowIterator<?>>> rowTransformer,
                                                       ReadExecutionController executionController)
     {
         assert executionController != null && executionController.validForReadOn(cfs);
@@ -745,7 +745,7 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
 
     private UnfilteredRowIterator queryMemtableAndDiskInternal(ColumnFamilyStore cfs,
                                                                ColumnFamilyStore.ViewFragment view,
-                                                               Function<Object, Transformation<BaseRowIterator<?>>> rowTransformer,
+                                                               Function<CellSourceIdentifier, Transformation<BaseRowIterator<?>>> rowTransformer,
                                                                ReadExecutionController controller)
     {
         /*
@@ -1021,7 +1021,7 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
      * no collection or counters are included).
      * This method assumes the filter is a {@code ClusteringIndexNamesFilter}.
      */
-    private UnfilteredRowIterator queryMemtableAndSSTablesInTimestampOrder(ColumnFamilyStore cfs, ColumnFamilyStore.ViewFragment view, Function<Object, Transformation<BaseRowIterator<?>>> rowTransformer, ClusteringIndexNamesFilter filter, ReadExecutionController controller)
+    private UnfilteredRowIterator queryMemtableAndSSTablesInTimestampOrder(ColumnFamilyStore cfs, ColumnFamilyStore.ViewFragment view, Function<CellSourceIdentifier, Transformation<BaseRowIterator<?>>> rowTransformer, ClusteringIndexNamesFilter filter, ReadExecutionController controller)
     {
         ImmutableBTreePartition result = null;
         SSTableReadMetricsCollector metricsCollector = new SSTableReadMetricsCollector();
